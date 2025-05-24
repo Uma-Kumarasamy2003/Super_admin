2c983b6d8e865ba9c2b4342bca193ef350085231
Uma-Kumarasamy2003 <uma@radiolinq.com>
2025-05-24 20:39:33 +0530

    first commit

--- src/components/layoutt/Header.js
+++ src/components/layoutt/Header.js
@@ -1,15 +1,21 @@
-// src/components/layoutt/Header.js
 import React from "react";
 import "../../index.css";
 import { LogoutOutlined } from "@ant-design/icons";
 import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/login");
+  };
+
   return (
     <div className="header">
       <span className="user-name">NIVETHITHA HEMACHANDRAN</span>
 
-      <Button type="primary">
+      <Button type="primary" onClick={handleLogout}>
         <LogoutOutlined />
         Logout
       </Button>
